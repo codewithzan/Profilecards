75ba966eb6d6b657776da3561454d00e0034bfcf
Bizan Rai <65980358+zanrai94@users.noreply.github.com>
2021-07-25 11:26:19 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+// Typeit
+new TypeIt("#type1", {
+  speed: 120,
+  loop: true,
+  waitUntilVisible: true,
+})
+  .type("Designer", { delay: 400 })
+  .pause(500)
+  .delete(9)
+  .type("Editor")
+  .pause(500)
+  .delete(9)
+  .go();
